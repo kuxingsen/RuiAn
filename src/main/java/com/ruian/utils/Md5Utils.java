a538633b8feb2afe3db28d26c7881a59a931cd33
kuexun <1158181372@qq.com>
2018-11-14 12:26:18 +0800

    修了个小bug

--- src/main/java/com/ruian/utils/Md5Utils.java
+++ src/main/java/com/ruian/utils/Md5Utils.java
@@ -5,7 +5,7 @@ import org.springframework.util.DigestUtils;
 public class Md5Utils{
 
 
-    private static final String salt = "henghePE";
+    private static final String salt = "ruian";
 
     public static String key(String adminId) {
         String key = adminId+'/'+salt;
